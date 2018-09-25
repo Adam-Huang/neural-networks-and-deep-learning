d5b5725a3fea26ae2e66ec1c30ac172c634d6717
KEDACOM Huang <245189274@qq.com>
2018-09-25 19:47:12 +0800

    用jupyter实现了network.py的程序

--- src/net_test.py
+++ src/net_test.py
@@ -4,7 +4,9 @@ import mnist_loader
 def exchange_data(traning_data):
     new_one = []
     for i in range(len(traning_data[0])):
-        new_one.append([traning_data[0][i],traning_data[1][i]])
+        temp=[0,0,0,0,0,0,0,0,0,0]
+        temp[traning_data[1][i]] = 1
+        new_one.append([traning_data[0][i],temp])
     return new_one
 
 if __name__=="__main__":
@@ -12,3 +14,8 @@ if __name__=="__main__":
     training = list(training)
     validation = list(validation)
     test = list(test)
+    tra = exchange_data(training)
+    val = exchange_data(validation)
+    tet = exchange_data(test)
+    ne = network.Network([784,30,10])
+    ne.SGD(tra,30,10,3.0,test_data=tet)
